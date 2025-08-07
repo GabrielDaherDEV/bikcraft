0f571a4169387e3500dc7436b75ee4000214bde2
gabrieldaher33@gmail.com <Gabriel Daher>
2025-08-07 16:41:54 -0300

    Interações JS / Animaçoes

---
+++ js/script.js
@@ -0,0 +1,71 @@
+// ATIVAR LINKS DO MENU
+const links = document.querySelectorAll(".header-menu a");
+
+function ativarLink(link) {
+  const url = location.href;
+  const href = link.href;
+
+  if (url.includes(href)) {
+    link.classList.add("ativo");
+  }
+}
+
+links.forEach(ativarLink);
+
+// ATIVAR ITENS DO ORÇAMENTO
+
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const element = document.getElementById(parametro);
+  if (element) {
+    element.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// PERGUNTAS FREQUENTES
+
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event) {
+  const pergunta = event.currentTarget;
+  const control = pergunta.getAttribute("aria-controls");
+  const resposta = document.getElementById(control);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa);
+  pergunta.setAttribute("aria-expanded", ativa);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener("click", ativarPergunta);
+}
+
+perguntas.forEach(eventosPerguntas);
+
+// GALERIA DE BICICLETAS
+const galeria = document.querySelectorAll(".bicicleta-imagens img");
+const galeriaContainer = document.querySelector(".bicicleta-imagens");
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+    galeriaContainer.prepend(img);
+  }
+  console.log(media);
+}
+
+function eventosGaleria(img) {
+  img.addEventListener("click", trocarImagem);
+}
+
+galeria.forEach(eventosGaleria);
+
+// Animação
+if (window.SimpleAnime) {
+  new SimpleAnime();
+}
